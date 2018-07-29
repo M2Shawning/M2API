f640898dce9da98fdd96d593fae938a281dba56a
Shawn Smith <m2shawning@gmail.com>
2018-07-29 04:22:36 -0500

    Added ChatPacket

---
+++ src/main/java/com/github/m2shawning/M2API/chatPacket/ChatPacket.java
@@ -0,0 +1,35 @@
+package com.github.m2shawning.M2API.chatPacket;
+
+import net.minecraft.server.v1_13_R1.IChatBaseComponent;
+import net.minecraft.server.v1_13_R1.PacketPlayOutTitle;
+import net.minecraft.server.v1_13_R1.PacketPlayOutTitle.EnumTitleAction;
+import org.bukkit.craftbukkit.v1_13_R1.entity.CraftPlayer;
+import org.bukkit.Bukkit;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class ChatPacket {
+
+    public static void sendTitlePackets(String displayText, ArrayList<UUID> playerUUIDs) {
+
+        PacketPlayOutTitle display = new PacketPlayOutTitle(EnumTitleAction.TITLE,
+                IChatBaseComponent.ChatSerializer.a("{\"text\":\"" +  displayText + "\"}"), 0, 0, 0);
+
+        for (UUID playerUUID : playerUUIDs) {
+            ((CraftPlayer) Bukkit.getServer().getPlayer(playerUUID))
+                    .getHandle().playerConnection.sendPacket(display);
+        }
+    }
+
+    public static void sendActionPackets(String displayText, ArrayList<UUID> playerUUIDs) {
+
+        PacketPlayOutTitle display = new PacketPlayOutTitle(EnumTitleAction.ACTIONBAR,
+                IChatBaseComponent.ChatSerializer.a("{\"text\":\"" +  displayText + "\"}"), 0, 0, 0);
+
+        for (UUID playerUUID : playerUUIDs) {
+            ((CraftPlayer) Bukkit.getServer().getPlayer(playerUUID))
+                    .getHandle().playerConnection.sendPacket(display);
+        }
+    }
+}
